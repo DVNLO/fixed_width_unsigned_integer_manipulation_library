2b2beb0769e92b46d976cb90ba67e51447835aba
Dan Vyenielo <dvyenielo@gmail.com>
2021-02-02 22:38:27 -0800

    Initial implementation for uint8. Tests for implementation pass.

---
+++ source/uint8.c
@@ -0,0 +1,6 @@
+#include "uint8.h"
+
+extern inline uint8_t get_bit_uint8(uint8_t const bit_idx, uint8_t const vals);
+extern inline uint8_t set_bit_uint8(uint8_t const bit_idx, uint8_t const vals);
+extern inline uint8_t clear_bit_uint8(uint8_t const bit_idx, uint8_t const vals);
+extern inline uint8_t toggle_bit_uint8(uint8_t const bit_idx, uint8_t const vals);
